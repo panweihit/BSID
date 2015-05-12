7c0ba82db7481897e93aae711724279039af7027
Wei Pan <panweihit@gmail.com>
2015-05-12 12:05:55 +0200

    Update GRN_dis.m

--- VanillaID/GRN_dis.m
+++ VanillaID/GRN_dis.m
@@ -1,9 +1,8 @@
-function [y, A,w_true] = GRN_dis(which)
+function [y, A,w_true] = GRN_dis(which,noise)
 ga1=0.3;ga2=0.4;ga3=0.5;ga4=0.2;ga5=0.4;ga6=0.6;
 be1=1.4;be2=1.5;be3=1.6;
 al1=4;al2=3;al3=5;
 h=4;
-lam=0*rand;
 % w=rand(6+h*12,6);
 % w_tru=[diag([-ga1,-ga2,-ga3,-ga4,-ga5,-ga6])+[zeros(3,3) diag([be1,be2,be3]);zeros(3,6)];[zeros(45,3);0 al2 0;0 0 al3;al1 0 0] zeros(48,3)];
 
@@ -22,7 +21,7 @@ samplerate = 1;
 for k=2:T %real time from 0:1:50
     %call hill function:H
     Phi(k-1,:)=[X(k-1,:) hill(X(k-1,:),1) hill(X(k-1,:),2) hill(X(k-1,:),3) hill(X(k-1,:),4)];
-    X(k,:)=X(k-1,:) + samplerate*Phi(k-1,:)*w_tru + lam*randn(1,6);
+    X(k,:)=X(k-1,:) + samplerate*Phi(k-1,:)*w_tru + noise*randn(1,6);
     Y(k-1,:)=  (X(k,:)-X(k-1,:))/samplerate; %M rows, derivative of x
 end
 
