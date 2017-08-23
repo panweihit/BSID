2e03de13efdfc15d3bd7a06ae8bcc10a708503fe
Wei Pan <panweihit@gmail.com>
2017-08-23 18:02:04 +0800

    Delete addwhite.m

--- HeteroID/subfunction/addwhite.m
+++
@@ -1,9 +0,0 @@
-function y = addwhite(y, perc)
-% perc=input('% of additive noise=');
-
-dev=std(y)*perc/100;
-noise=randn(size(y)).*kron(dev,ones(size(y,1),1));
-y=y+noise;
-
-% save corrupted y perc;
- 
